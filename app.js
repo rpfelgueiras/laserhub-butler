cef6ad8486138ed63dd9bbab4f49770b6d3797fc
Rui Felgueiras <r.felgueiras@laserhub.com>
2023-02-22 15:21:09 +0000

    refactor

--- app.js
+++ app.js
@@ -11,18 +11,20 @@ const app = new App({
 
 app.event("app_mention", async ({ event, context, client, body, say }) => {
   try {
-    var butlerRules = require("./butler-rules");
+    var butler = require("./butler");
 
-    if (!butlerRules.isThis.theRightCommand(event.text)) {
+    if (!butler.isThis.theRightCommand(event.text)) {
       await say(
-        `<@${event.user}> I can't recognize that order. I am sorry! Want to try again? :pray:`
+        // `<@${event.user}> I can't recognize that order. I am sorry! Want to try again? :pray:`
+        `I can't recognize that order. I am sorry! Want to try again? :pray:`
       );
       return;
     }
 
-    if (!butlerRules.isThis.theRightChannel(event.channel)) {
+    if (!butler.isThis.theRightChannel(event.channel)) {
       await say(
-        `<@${event.user}> wrong channel! :cry: I can only open the door in the #office-stuttgart channel.`
+        // `<@${event.user}> wrong channel! :cry: I can only open the door in the #office-stuttgart channel.`
+        `wrong channel! :cry: I can only open the door in the #office-stuttgart channel.`
       );
       return;
     }
@@ -38,16 +40,7 @@ app.event("app_mention", async ({ event, context, client, body, say }) => {
     var now = moment().utc();
     var userDate = now.tz(userWhoSentMessage.user.tz);
 
-    // var inRange = isInRange(userDate.getTime(), range);
-    // if (!inRange) {
-    //   await say(
-    //     // `<@${event.user}> I am not allowed to open the door at this time :pray: I am sorry!.`
-    //     `I am not allowed to open the door at this time :pray: I am sorry!.`
-    //   );
-    //   return;
-    // }
-
-    if (!butlerRules.isThis.theRightTime(userDate.format('HH:mm'))) {
+    if (!butler.isThis.theRightTime(userDate.format("HH:mm"))) {
       await say(
         // `<@${event.user}> I am not allowed to open the door at this time :pray: I am sorry!.`
         `I am not allowed to open the door at this time :pray: I am sorry!`
@@ -61,28 +54,6 @@ app.event("app_mention", async ({ event, context, client, body, say }) => {
       // `<@${event.user}> I will open the door! Give me some seconds :run:`
       `I will open the door! Give me some seconds :run:`
     );
-
-    // await say({
-    //   blocks: [
-    //     {
-    //       type: "section",
-    //       text: {
-    //         type: "mrkdwn",
-    //         text: `Thanks for the mention <@${event.user}>! Here's a button`,
-    //       },
-    //       accessory: {
-    //         type: "button",
-    //         text: {
-    //           type: "plain_text",
-    //           text: "Button",
-    //           emoji: true,
-    //         },
-    //         value: "click_me_123",
-    //         action_id: "first_button",
-    //       },
-    //     },
-    //   ],
-    // });
   } catch (error) {
     console.error(error);
   }
@@ -91,50 +62,7 @@ app.event("app_mention", async ({ event, context, client, body, say }) => {
 (async () => {
   const port = 3000;
 
-  // Start your app
+  // Start the app
   await app.start(process.env.PORT || port);
   console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
 })();
-
-var range = ["08:00", "22:00"];
-
-function isInRange(value, range) {
-  return value >= range[0] && value <= range[1];
-}
-
-// ["04:59", "08:30", "23:15"].forEach(function (time) {
-//   console.log(
-//     time + " is " + (isInRange(time, range) ? " " : "not ") + "in range"
-//   );
-// });
-
-// // Alternatively
-// ["04:59", "23:15", "08:30"].forEach(function (time) {
-//   var inRange = isInRange(time, range);
-//   console.log(
-//     time +
-//       " is in range " +
-//       (inRange ? range : range.slice().reverse()).join(" - ")
-//   );
-// });
-
-app.message(/^(hi|hello|hey).*/, async ({ context, say }) => {
-  // RegExp matches are inside of context.matches
-  const greeting = context.matches[0];
-
-  await say(`${greeting}, how are you?`);
-});
-
-// This will match any message that contains 👋
-app.message(":wave:", async ({ message, say }) => {
-  await say(`Hello, <@${message.user}>`);
-});
-
-app.message("/^(open the door).*/", async ({ message, say }) => {
-  // await say(`Door, <@${message.user}>`);
-  console.log(message);
-
-  // call remote door opener
-
-  // return success or not :)
-});
--- butler.js
+++ butler.js
@@ -1,80 +1,90 @@
-// require("dotenv").config();
+require("dotenv").config();
 // var request = require("request");
 
-// function openDoor(location, callback) {
-//   // var appId = 'aa0f1b0be45dca476178787f941c76dc'; // This is a managed key - I don't mind if you steal it ;)
-//   // var url = 'http://api.openweathermap.org/data/2.5/weather?q='+location+'&appid='+appId;
-//   // request({
-//   //     url: url,
-//   //     json: true
-//   // }, function (error, response, body) {
-//   //     if (!error && response.statusCode === 200) {
-//   //         callback(processResults(body));
-//   //     }
-//   // });
+function openDoor(location, callback) {
+  // var appId = 'aa0f1b0be45dca476178787f941c76dc'; // This is a managed key - I don't mind if you steal it ;)
+  // var url = 'http://api.openweathermap.org/data/2.5/weather?q='+location+'&appid='+appId;
+  // request({
+  //     url: url,
+  //     json: true
+  // }, function (error, response, body) {
+  //     if (!error && response.statusCode === 200) {
+  //         callback(processResults(body));
+  //     }
+  // });
+}
+
+// function processResults(allResults){
+//     return {
+//         'minTemp'   : kelvinToCelcius(allResults.main.temp_min),    // Celsius
+//         'maxTemp'   : kelvinToCelcius(allResults.main.temp_max),    // Celsius
+//         'chanceRain': 0.83,                                         // 0 - 1 //TODO find new api
+//         'rainFall'  : getRainFall(allResults.rain),                 // mm
+//         'cloudCover': allResults.clouds.all                         // percentage
+//     }
 // }
 
-// // function processResults(allResults){
-// //     return {
-// //         'minTemp'   : kelvinToCelcius(allResults.main.temp_min),    // Celsius
-// //         'maxTemp'   : kelvinToCelcius(allResults.main.temp_max),    // Celsius
-// //         'chanceRain': 0.83,                                         // 0 - 1 //TODO find new api
-// //         'rainFall'  : getRainFall(allResults.rain),                 // mm
-// //         'cloudCover': allResults.clouds.all                         // percentage
-// //     }
-// // }
+// function kelvinToCelcius(kTemp){
+//     return Math.round(kTemp - 273);
+// }
 
-// // function kelvinToCelcius(kTemp){
-// //     return Math.round(kTemp - 273);
-// // }
+// function getRainFall(rainObj){
+//     if (!rainObj) return 0;
+//     return (rainObj['1h'] || rainObj['2h'] || rainObj['3h'] || 0);
+// }
 
-// // function getRainFall(rainObj){
-// //     if (!rainObj) return 0;
-// //     return (rainObj['1h'] || rainObj['2h'] || rainObj['3h'] || 0);
-// // }
+exports.openDoor = openDoor;
 
-// exports.openDoor = openDoor;
-
-// // module.exports.isThis = {
-// //   /**
-// //    *
-// //    * @param {string} command
-// //    * @returns {boolean}
-// //    */
-// //   theRightCommand: function (command) {
-// //     return command.indexOf("open the door") < 0 ? false : true;
-// //   },
-
-// //   /**
-// //    *
-// //    * @param {string} channel
-// //    * @returns {boolean}
-// //    */
-// //   theRightChannel: function (channel) {
-// //     return channel !== process.env.SLACK_STUTTGART_OFFICE_CHANNEL_ID
-// //       ? false
-// //       : true;
-// //   },
-
-// //   /**
-// //    * 
-// //    * @param {*} time 
-// //    * @returns 
-// //    */
-// //   theRightDay: function (time) {
-// //     return isInRange(time);
-// //   },
-
-// //   /**
-// //    * 
-// //    * @returns 
-// //    */
-// //   theRightTime: function () {
-// //     return false;
-// //   },
-// // };
-
-// // function isInRange(value) {
-// //   var range = ["08:00", "22:00"];
-// //   return value >= range[0] && value <= range[1];
-// // }
+module.exports.isThis = {
+    /**
+     * Parses the command text to check if it is the right command to open the door
+     * @param {string} command Text message containing the command
+     * @returns {boolean} True if it the command is parsed the right way
+     */
+    theRightCommand: function (command) {
+      return command.indexOf("open the door") < 0 ? false : true;
+    },
+  
+    /**
+     *
+     * @param {string} channel
+     * @returns {boolean}
+     */
+    theRightChannel: function (channel) {
+      return channel !== process.env.SLACK_STUTTGART_OFFICE_CHANNEL_ID
+        ? false
+        : true;
+    },
+  
+    /**
+     *
+     * @param {*} time
+     * @returns
+     */
+    theRightTiming: function (time) {
+      return theRightTime(time);
+    },
+  
+    /**
+     * Check the time between 8 AM and 6 PM
+     * @param {*} time
+     * @returns
+     */
+    between8AmAnd6Pm: function (time) {
+      return isInRange(time, eightAmAndSixPm);
+    },
+  
+    /**
+     *
+     * @returns
+     */
+    theRightDay: function () {
+      return false;
+    },
+  };
+  
+  function isInRange(value, eightAmAndSixPm) {
+    return value >= eightAmAndSixPm[0] && value <= eightAmAndSixPm[1];
+  }
+  
+  const eightAmAndSixPm = ["08:00", "18:00"];
