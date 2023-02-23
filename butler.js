d4546765ba5042d24d1434686c5531e972e838c1
Rui Felgueiras <r.felgueiras@laserhub.com>
2023-02-23 09:30:44 +0000

    add unit tests

--- butler.js
+++ butler.js
@@ -36,56 +36,56 @@ function openDoor(location, callback) {
 exports.openDoor = openDoor;
 
 module.exports.isThis = {
-    /**
-     * Parses the command text to check if it is the right command to open the door
-     * @param {string} command Text message containing the command
-     * @returns {boolean} True if it the command is parsed the right way
-     */
-    theRightCommand: function (command) {
-      return command.indexOf("open the door") < 0 ? false : true;
-    },
-  
-    /**
-     *
-     * @param {string} channel
-     * @returns {boolean}
-     */
-    theRightChannel: function (channel) {
-      return channel !== process.env.SLACK_STUTTGART_OFFICE_CHANNEL_ID
-        ? false
-        : true;
-    },
-  
-    /**
-     *
-     * @param {*} time
-     * @returns
-     */
-    theRightTiming: function (day, time) {
-      return this.between8AmAnd6Pm(time) && this.theRightDay(day);
-    },
-  
-    /**
-     * Check the time between 8 AM and 6 PM
-     * @param {*} time
-     * @returns
-     */
-    between8AmAnd6Pm: function (time) {
-      return isInRange(time, eightAmAndSixPm);
-    },
-  
-    /**
-     *
-     * @returns
-     */
-    theRightDay: function (day) {
-        return isInRange(day, weekDays);
-    },
-  };
-  
-  function isInRange(value, range) {
-    return value >= range[0] && value <= range[range.length - 1]
-  }
-  
-  const eightAmAndSixPm = ["08:00", "22:00"];
-  const weekDays = [1, 2, 3, 4, 5, 6];
+  /**
+   * Parses the command text to check if it is the right command to open the door
+   * @param {string} command Text message containing the command
+   * @returns {boolean} True if it the command is parsed the right way
+   */
+  theRightCommand: function (command) {
+    return command.indexOf("open the door") < 0 ? false : true;
+  },
+
+  /**
+   *
+   * @param {string} channel
+   * @returns {boolean}
+   */
+  theRightChannel: function (channel) {
+    return channel !== process.env.SLACK_STUTTGART_OFFICE_CHANNEL_ID
+      ? false
+      : true;
+  },
+
+  /**
+   *
+   * @param {*} time
+   * @returns
+   */
+  theRightTiming: function (day, time) {
+    return this.between8AmAnd5Pm(time) && this.theRightDay(day);
+  },
+
+  /**
+   * Check the time between 8 AM and 6 PM
+   * @param {*} time
+   * @returns
+   */
+  between8AmAnd5Pm: function (time) {
+    return isInRange(time, eightAmAndSixPm);
+  },
+
+  /**
+   *
+   * @returns
+   */
+  theRightDay: function (day) {
+    return isInRange(day, weekDays);
+  },
+};
+
+function isInRange(value, range) {
+  return value >= range[0] && value <= range[range.length - 1];
+}
+
+const eightAmAndSixPm = ["08:00", "17:00"];
+const weekDays = [1, 2, 3, 4, 5];
---
+++ test/test-butler.js
@@ -0,0 +1,72 @@
+var chai = require("chai");
+var expect = chai.expect;
+var should = chai.should();
+var assert = chai.assert;
+
+var butler = require("../butler");
+
+describe("Test butler's basic functionality", function () {
+  it("should return true if it is an open door command", function () {
+    expect(butler.isThis.theRightCommand("open the door")).to.be.a("boolean");
+    expect(butler.isThis.theRightCommand("open the door")).equal(true);
+
+    expect(butler.isThis.theRightCommand("bla bla open the door bla bla")).to.be.a("boolean");
+    expect(butler.isThis.theRightCommand("bla bla open the door bla bla")).equal(true);
+
+    expect(butler.isThis.theRightCommand("command invalid")).to.be.a("boolean");
+    expect(butler.isThis.theRightCommand("command invalid")).equal(false);
+  });
+
+  it("should return true if it is the right Slack channel", function () {
+    expect(butler.isThis.theRightChannel("C042GKBDRJR")).to.be.a("boolean");
+    expect(butler.isThis.theRightChannel("C042GKBDRJR")).equal(true);
+
+    expect(butler.isThis.theRightChannel("1111")).to.be.a("boolean");
+    expect(butler.isThis.theRightChannel("1111")).equal(false);
+  });
+
+  it("should return true if it is weekday and work hours", function () {
+    // Monday
+    expect(butler.isThis.theRightTiming(1, "09:00")).to.be.a("boolean");
+    expect(butler.isThis.theRightTiming(1, "09:00")).equal(true);
+
+    expect(butler.isThis.theRightTiming(1, "07:59")).to.be.a("boolean");
+    expect(butler.isThis.theRightTiming(1, "07:59")).equal(false);
+
+    expect(butler.isThis.theRightTiming(1, "08:00")).to.be.a("boolean");
+    expect(butler.isThis.theRightTiming(1, "08:00")).equal(true);
+
+    expect(butler.isThis.theRightTiming(1, "16:59")).to.be.a("boolean");
+    expect(butler.isThis.theRightTiming(1, "16:59")).equal(true);
+
+    expect(butler.isThis.theRightTiming(1, "17:00")).to.be.a("boolean");
+    expect(butler.isThis.theRightTiming(1, "17:00")).equal(true);
+
+    expect(butler.isThis.theRightTiming(1, "17:01")).to.be.a("boolean");
+    expect(butler.isThis.theRightTiming(1, "17:01")).equal(false);
+
+    // Tuesday
+    expect(butler.isThis.theRightTiming(2, "09:00")).to.be.a("boolean");
+    expect(butler.isThis.theRightTiming(2, "09:00")).equal(true);
+
+    // Wednesday
+    expect(butler.isThis.theRightTiming(3, "09:00")).to.be.a("boolean");
+    expect(butler.isThis.theRightTiming(3, "09:00")).equal(true);
+
+    // Thursday
+    expect(butler.isThis.theRightTiming(4, "09:00")).to.be.a("boolean");
+    expect(butler.isThis.theRightTiming(4, "09:00")).equal(true);
+
+    // Friday
+    expect(butler.isThis.theRightTiming(5, "09:00")).to.be.a("boolean");
+    expect(butler.isThis.theRightTiming(5, "09:00")).equal(true);
+
+    // Saturday
+    expect(butler.isThis.theRightTiming(6, "09:00")).to.be.a("boolean");
+    expect(butler.isThis.theRightTiming(6, "09:00")).equal(false);
+
+    // Sunday
+    expect(butler.isThis.theRightTiming(7, "09:00")).to.be.a("boolean");
+    expect(butler.isThis.theRightTiming(7, "09:00")).equal(false);
+  });
+});
